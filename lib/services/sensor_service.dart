6777c7e2e03a7b1136988a9184c41914cfd1fb27
unknown <kwok_leong1985@hotmail.com>
2026-02-23 10:19:07 +0800

    Switch to standalone HTML for better iOS support

--- lib/services/sensor_service.dart
+++
@@ -1,6 +0,0 @@
-import 'package:sensors_plus/sensors_plus.dart';
-
-class SensorService {
-  // We expose the stream so the UI can "listen" to it
-  Stream<AccelerometerEvent> get accelerometerEvents => accelerometerEventStream();
-}
