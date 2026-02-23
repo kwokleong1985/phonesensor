4a0e642c9fc45ce8216dfecf6dd77336140a99fe
unknown <kwok_leong1985@hotmail.com>
2026-02-23 10:24:32 +0800

    removed tracked

--- deploy.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Deploying Simple Web App to GitHub...
-git add index.html
-git commit -m "Switch to standalone HTML for better iOS support"
-git push origin main -f
-echo Done! Your site should update in a minute.
